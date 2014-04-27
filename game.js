349a593b71f10790fca01714240a7d0adc2bdd59
mindyk <michael.indyk@googlemail.com>
2014-04-27 21:42:06 +0200

    init

---
+++ game.js
@@ -0,0 +1,358 @@
+var G = {
+
+	w: 800,
+	h: 600,
+
+	ctx: null,
+	bgImg: null,
+
+	dX: null,
+	dY: null,
+	gX: null,
+	gY: null,
+
+	interval: null,
+
+	vel: null,
+	_state: null,
+	tick: 0,
+
+	VEL_IDLE: -0.5,
+	VEl_DIVE: 7,
+	VEL_SURFACE: -7,
+	VEL_PAUSE: 0,
+
+	entities: [],
+
+	init: function (ctx, bgImg) {
+		//console.log('G.init');
+		G.ctx = ctx;
+		G.bgImg = bgImg;
+
+		G.dX = 0;
+		G.dY = 0;
+		G.gX = 0;
+		G.gY = 0;
+
+		G.vel = G.VEL_IDLE;
+
+		G.state("init");
+
+		$(document).keydown(G.input);
+		P.init(ctx, G.gY);
+		G.draw();
+	},
+
+	state: function (value) {
+
+		if (typeof value == "undefined") {
+			return G._state;
+		}
+		//console.log(value, 'state');
+		G._state = value;
+	},
+
+	start: function () {
+		//console.log('G.start');
+		G.state("start");
+		G.interval = setInterval(G.update, Math.floor(1000 / 60));
+	},
+
+	draw: function () {
+		G.drawBg();
+
+		/**
+		G.ctx.fillStyle = 'black';
+		G.ctx.font = "bold 16px Arial";
+		G.ctx.fillText(G.gY, 10, 25);
+		 **/
+
+		if (P.MEM_DIVE_BREACH == true) {
+			/*
+			G.ctx.fillStyle = 'black';
+			G.ctx.font = "bold 16px Arial";
+			G.ctx.fillText("DIVE BREACHED", 10, 50);
+			*/
+
+			G.ctx.fillStyle = 'black';
+			G.ctx.font = "bold 16px Arial";
+			G.ctx.fillText(P.MEM_HOW_DEEP - 600, 300, P.dY - 20);
+			if (P.state == "dead") {
+				G.ctx.beginPath();
+				G.ctx.lineWidth = 1;
+				G.ctx.moveTo(300, P.dY - 25);
+				G.ctx.lineTo(350, P.dY - 25);
+				G.ctx.strokeStyle = "black";
+				G.ctx.stroke();
+			}
+		}
+
+
+		if (P.MEM_SURFACE_BREACH == true) {
+			G.ctx.fillStyle = 'white';
+			G.ctx.font = "bold 40px Arial";
+			G.ctx.fillText("PRESS F5 to try again", 100, 75);
+		}
+
+
+		/**
+		G.ctx.fillStyle = 'black';
+		G.ctx.font = "bold 16px Arial";
+		G.ctx.fillText(P.air, 100, 25);
+		 **/
+
+		for (var mI = 0; mI <= G.entities.length - 1; mI++) {
+			G.entities[mI].draw();
+		}
+
+		if (P.MEM_SURFACE_BREACH) {
+			P.draw(350, G.h - G.gY);
+		} else {
+			P.draw(350, 100);
+		}
+	},
+
+	drawBg: function () {
+		//console.log('G.drawBg');
+		var dX = 0, dY = 0;
+
+		G.ctx.clearRect(0, 0, G.w, G.h);
+
+
+
+		if (G.gY > 0) {
+			dY -= G.gY;
+		}
+
+		if (G.gY < G.h) {
+			G.ctx.drawImage(G.bgImg, 0, dY);
+		}
+
+		var my_gradient = G.ctx.createLinearGradient(0, 0, 0, G.h);
+		my_gradient.addColorStop(0, "rgb(202, 228, 241)");
+		my_gradient.addColorStop(1, "rgb(0, 87, 138)");
+		G.ctx.fillStyle = my_gradient;
+
+		if (G.gY < G.h) {
+			G.ctx.fillRect(0, 600 - G.gY, 800, 600);
+		} else {
+			G.ctx.fillRect(0, 0, 800, 600);
+		}
+
+		if (G.state() == 'init') {
+			//console.log('DRAW START');
+			G.ctx.fillStyle = 'black';
+			G.ctx.font = "bold 40px Arial";
+			G.ctx.fillText("PRESS SPACE", 250, 250);
+		}
+
+		if (P.MEM_DIVE_BREACH && P.MEM_PRESSED_DIVE == false && G.tick < 200) {
+			G.ctx.fillStyle = 'white';
+			G.ctx.font = "bold 40px Arial";
+			G.ctx.fillText("PRESS S to dive ", 200, 250);
+		}
+
+		if (P.MEM_PRESSED_DIVE && P.MEM_PRESSED_SURFACE == false && G.tick < 250) {
+			G.ctx.fillStyle = 'white';
+			G.ctx.font = "bold 40px Arial";
+			G.ctx.fillText("PRESS W to surface", 180, 250);
+		}
+
+		if (P.state == "dead") {
+
+			G.ctx.fillStyle = 'white';
+			if (G.dY < 500) {
+				G.ctx.fillStyle = 'black';
+			}
+			G.ctx.font = "bold 40px Arial";
+			G.ctx.fillText("DROWNED", 300, 250);
+		}
+	},
+
+	goingDown: function () {
+		return this.vel > 0;
+	},
+
+	update: function () {
+		if (G.state() == "pause") {
+			return;
+		}
+
+		G.tick += 1;
+
+		if (P.MEM_SURFACE_BREACH == true) {
+			G.vel = G.VEL_IDLE;
+			if (G.gY == 100) {
+				G.vel = G.VEL_PAUSE;
+			}
+		}
+
+		G.gY += G.vel;
+
+		if (G.gY > 100) {
+
+			if (P.MEM_SURFACE_BREACH != true) {
+				if (G.gY % 200 == 0) {
+					//console.log('spawn fish');
+					fx = Math.floor((Math.random() * 800) + 1);
+					G.entities.push(new fish(G.ctx, fx, G.gY + G.h));
+				}
+			}
+
+			if (G.tick % 100 == 0) {
+				var bX, bR;
+				bx = Math.floor((Math.random() * 800) + 1);
+				bR = Math.floor((Math.random() * 10) + 1);
+				G.entities.push(new bubble(G.ctx, bx, G.gY + G.h, bR));
+			}
+		}
+
+		P.update();
+
+		for (var mI = 0; mI <= G.entities.length - 1; mI++) {
+			G.entities[mI].update();
+		}
+
+		G.draw();
+		G.cleanup();
+	},
+
+	input: function (e) {
+		//console.log(e.which, 'G.input');
+		switch (e.which) {
+			case 32:
+				switch (G.state()) {
+					case "init":
+						G.start();
+						break;
+					case "pause":
+						//G.resume();
+						break;
+					case "start":
+						//G.stop();
+						break;
+				}
+				break;
+			case 83:
+
+				break;
+			case 87:
+				break;
+			case 27:
+				//G.state("pause");
+				break;
+			default:
+				break;
+		}
+	},
+
+	log: function (value) {
+		//console.log(value);
+	},
+
+	stop: function () {
+		G.vel = 0;
+		//G.state("pause");
+	},
+
+	resume: function () {
+		G.state("start");
+	},
+
+	cleanup: function () {
+		for (var i = 0; i < G.entities.length - 1; i++) {
+			var cBubble = G.entities[i];
+			if (cBubble.y < 600) {
+				G.entities.splice(i, 1);
+			}
+		}
+	}
+};
+
+var fish = function (ctx, x, y) {
+	this.ctx = ctx;
+	this.x = x;
+	this.y = y;
+	this._draw = false;
+	this.align = "left";
+	//console.log(y, 'fish');
+
+	this.draw = function () {
+		if (this._draw == false) {
+			return;
+		}
+		var y = this.y - G.gY;
+
+		// body
+		this.ctx.fillStyle = "rgb(100, 100, 100)";
+		this.ctx.fillRect(this.x, y, 30, 20);
+		// tail
+		this.ctx.fillStyle = "rgb(100, 100, 100)";
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.x + 30, y + 10);
+		this.ctx.lineTo(this.x + 40, y); // /
+		this.ctx.lineTo(this.x + 40, y + 20); // |
+		this.ctx.lineTo(this.x + 30, y + 10); // \
+		//this.ctx.stroke();
+		this.ctx.fill();
+		this.ctx.closePath();
+
+
+		// eye
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = "rgb(255, 255, 255)";
+		this.ctx.arc(this.x + 10, y + 10, 5, 0, 2 * Math.PI, false);
+		this.ctx.closePath();
+		this.ctx.stroke();
+	};
+
+	this.update = function () {
+		this._draw = (this.y < G.gY + G.h && this.y > G.gY);
+
+		if (this.align == "left") {
+			if (this.x > 0) {
+				this.x -= 1;
+			} else {
+				this.align = "right";
+			}
+		}
+
+		if (this.align == "right") {
+			if (this.x < G.w) {
+				this.x += 1;
+			} else {
+				this.align = "left";
+			}
+		}
+	};
+
+
+};
+
+var bubble = function (ctx, x, y, r) {
+	this.ctx = ctx;
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this._draw = false;
+	this.vel = Math.floor((Math.random() * 3) + 1);
+
+
+	this.draw = function () {
+
+		if (this._draw == false) {
+			return;
+		}
+		var y = this.y - G.gY;
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = "rgb(255, 255, 255)";
+		this.ctx.arc(this.x, y, this.r, 0, 2 * Math.PI, false)
+		this.ctx.stroke();
+	};
+
+	this.update = function (v) {
+		this._draw = (this.y < G.gY + G.h && this.y > G.gY);
+		this.y -= this.vel;
+	}
+
+};
---
+++ player.js
@@ -0,0 +1,172 @@
+var P = {
+	ctx : null,
+	dX: 350,
+	dY: 150,
+
+	gY : null,
+
+	MEM_JUMPED : false,
+	MEM_DIVE_BREACH : false,
+	MEM_SURFACE_BREACH: false,
+	MEM_PRESSED_DIVE: false,
+	MEM_PRESSED_SURFACE: false,
+	MEM_HOW_DEEP: 0,
+
+	air : null,
+	breath: null,
+
+
+
+	init: function (ctx, gY) {
+		//console.log(gY, 'player.init');
+		P.ctx = ctx;
+		P.gY = gY;
+		P.air = 150;
+
+
+		P.setState("idle");
+
+		$(document).keydown(P.input);
+		P.breath = setInterval(function () {
+			if (P.MEM_DIVE_BREACH == true && P.MEM_SURFACE_BREACH == false) {
+				P.drain(10)
+			}
+		}, 1000);
+	},
+
+	drain : function (value) {
+		P.air -= value;
+		if (P.air <= 0) {
+			P.die();
+		}
+	},
+
+	input: function (e) {
+
+		switch (e.which) {
+			case 65:
+				//console.log('A');
+				//P.x -= 10;
+				break;
+			case 68:
+				//console.log('D');
+				//P.x += 10;
+				break;
+			case 83:
+				//console.log('S');
+				if (P.state != "dead") {
+					P.dive(G.VEl_DIVE, 200);
+				}
+				P.MEM_PRESSED_DIVE = true;
+				break;
+			case 87:
+				//console.log('W');
+				P.MEM_PRESSED_SURFACE = true;
+				if (P.state != "dead") {
+					P.surface();
+				}
+				break;
+			default :
+				//console.log(e.which, 'player.init');
+				break;
+		}
+
+	},
+
+	moveTo: function (x, y) {
+		P.dX = x;
+		P.dY = y;
+	},
+
+	dive: function (vel, dest) {
+		if (P.state == "dead") {
+			return;
+		}
+
+		if (P.state == 'idle') {
+			P.setState('diving');
+			P.diveTo = P.gY + dest;
+			P.drain(5);
+		}
+		if (P.gY < P.diveTo) {
+			G.vel = vel;
+		} else {
+			P.diveTo = null;
+			P.setState('idle');
+			G.vel = G.VEL_IDLE;
+		}
+	},
+
+	surface: function () {
+
+		if (P.state == 'idle') {
+			P.setState('rising');
+			P.diveTo = P.gY - 200;
+
+		}
+
+		if (P.gY > P.diveTo) {
+			G.vel = G.VEL_SURFACE;
+		} else {
+			P.diveTo = null;
+			P.setState('idle');
+			G.vel = G.VEL_IDLE;
+		}
+
+	},
+
+	die : function () {
+		P.setState("dead");
+		G.vel = G.VEL_IDLE;
+	},
+
+	update: function () {
+		P.gY = G.gY + P.dY;
+
+		if (P.gY > P.MEM_HOW_DEEP) {
+			P.MEM_HOW_DEEP = P.gY;
+		}
+
+		if (P.gY > G.h) {
+			P.MEM_DIVE_BREACH = true;
+		}
+
+		if (P.gY < G.h && P.MEM_DIVE_BREACH) {
+			P.MEM_SURFACE_BREACH = true;
+
+		}
+
+		if (P.MEM_SURFACE_BREACH) {
+			P.air = 100;
+		}
+
+		if (P.MEM_JUMPED == false) {
+			P.dive(20, G.h);
+			P.MEM_JUMPED = true;
+		}
+
+		if (P.state == "diving") {
+			P.dive(G.VEl_DIVE, 200);
+		}
+
+		if (P.state == 'rising' && P.MEM_SURFACE_BREACH == false) {
+			P.surface();
+		}
+	},
+
+	draw: function (dX, dY) {
+		P.dY = dY;
+		//console.log('player.draw');
+		P.ctx.fillStyle = "rgb(0, 0, 0)";
+		if (P.state == 'dead') {
+			P.ctx.fillRect(dX, dY, 100, 30);
+		} else {
+			P.ctx.fillRect(dX, dY, 30, 100);
+		}
+	},
+
+	setState: function (value) {
+		//console.log(value, "player.setState");
+		P.state = value;
+	}
+};
